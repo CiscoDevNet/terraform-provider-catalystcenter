419c11fb5b3e8118fddc9c24a104d2c90dc415cb
Rafal Slominski <rslominski@gmail.com>
2025-06-24 14:23:26 +0200

    make minimal test a bit bigger

--- internal/provider/resource_catalystcenter_wireless_rf_profile_test.go
+++ internal/provider/resource_catalystcenter_wireless_rf_profile_test.go
@@ -32,8 +32,27 @@ func TestAccCcWirelessRFProfile(t *testing.T) {
 	checks = append(checks, resource.TestCheckResourceAttr("catalystcenter_wireless_rf_profile.test", "rf_profile_name", "RF_Profile_1"))
 	checks = append(checks, resource.TestCheckResourceAttr("catalystcenter_wireless_rf_profile.test", "default_rf_profile", "false"))
 	checks = append(checks, resource.TestCheckResourceAttr("catalystcenter_wireless_rf_profile.test", "enable_radio_type_a", "true"))
-	checks = append(checks, resource.TestCheckResourceAttr("catalystcenter_wireless_rf_profile.test", "enable_radio_type_b", "false"))
+	checks = append(checks, resource.TestCheckResourceAttr("catalystcenter_wireless_rf_profile.test", "enable_radio_type_b", "true"))
 	checks = append(checks, resource.TestCheckResourceAttr("catalystcenter_wireless_rf_profile.test", "enable_radio_type6_g_hz", "false"))
+	checks = append(checks, resource.TestCheckResourceAttr("catalystcenter_wireless_rf_profile.test", "radio_type_a_parent_profile", "CUSTOM"))
+	checks = append(checks, resource.TestCheckResourceAttr("catalystcenter_wireless_rf_profile.test", "radio_type_a_radio_channels", "36,40,44,48,124,128,157,161"))
+	checks = append(checks, resource.TestCheckResourceAttr("catalystcenter_wireless_rf_profile.test", "radio_type_a_data_rates", "6,9,12,18,24,36,48,54"))
+	checks = append(checks, resource.TestCheckResourceAttr("catalystcenter_wireless_rf_profile.test", "radio_type_a_mandatory_data_rates", "6"))
+	checks = append(checks, resource.TestCheckResourceAttr("catalystcenter_wireless_rf_profile.test", "radio_type_a_power_threshold_v1", "-60"))
+	checks = append(checks, resource.TestCheckResourceAttr("catalystcenter_wireless_rf_profile.test", "radio_type_a_rx_sop_threshold", "CUSTOM"))
+	checks = append(checks, resource.TestCheckResourceAttr("catalystcenter_wireless_rf_profile.test", "radio_type_a_min_power_level", "8"))
+	checks = append(checks, resource.TestCheckResourceAttr("catalystcenter_wireless_rf_profile.test", "radio_type_a_max_power_level", "20"))
+	checks = append(checks, resource.TestCheckResourceAttr("catalystcenter_wireless_rf_profile.test", "radio_type_a_channel_width", "20"))
+	checks = append(checks, resource.TestCheckResourceAttr("catalystcenter_wireless_rf_profile.test", "radio_type_a_custom_rx_sop_threshold", "-70"))
+	checks = append(checks, resource.TestCheckResourceAttr("catalystcenter_wireless_rf_profile.test", "radio_type_b_parent_profile", "HIGH"))
+	checks = append(checks, resource.TestCheckResourceAttr("catalystcenter_wireless_rf_profile.test", "radio_type_b_radio_channels", "1,6,11"))
+	checks = append(checks, resource.TestCheckResourceAttr("catalystcenter_wireless_rf_profile.test", "radio_type_b_data_rates", "1,2,5.5,6,9,11,12,18,24,36,48,54"))
+	checks = append(checks, resource.TestCheckResourceAttr("catalystcenter_wireless_rf_profile.test", "radio_type_b_mandatory_data_rates", "1,2"))
+	checks = append(checks, resource.TestCheckResourceAttr("catalystcenter_wireless_rf_profile.test", "radio_type_b_power_threshold_v1", "-60"))
+	checks = append(checks, resource.TestCheckResourceAttr("catalystcenter_wireless_rf_profile.test", "radio_type_b_rx_sop_threshold", "CUSTOM"))
+	checks = append(checks, resource.TestCheckResourceAttr("catalystcenter_wireless_rf_profile.test", "radio_type_b_min_power_level", "8"))
+	checks = append(checks, resource.TestCheckResourceAttr("catalystcenter_wireless_rf_profile.test", "radio_type_b_max_power_level", "20"))
+	checks = append(checks, resource.TestCheckResourceAttr("catalystcenter_wireless_rf_profile.test", "radio_type_b_custom_rx_sop_threshold", "-70"))
 
 	var steps []resource.TestStep
 	steps = append(steps, resource.TestStep{
@@ -59,7 +78,7 @@ func testAccCcWirelessRFProfileConfig_minimum() string {
 	config += `	rf_profile_name = "RF_Profile_1"` + "\n"
 	config += `	default_rf_profile = false` + "\n"
 	config += `	enable_radio_type_a = true` + "\n"
-	config += `	enable_radio_type_b = false` + "\n"
+	config += `	enable_radio_type_b = true` + "\n"
 	config += `	enable_radio_type6_g_hz = false` + "\n"
 	config += `}` + "\n"
 	return config
@@ -73,8 +92,27 @@ func testAccCcWirelessRFProfileConfig_all() string {
 	config += `	rf_profile_name = "RF_Profile_1"` + "\n"
 	config += `	default_rf_profile = false` + "\n"
 	config += `	enable_radio_type_a = true` + "\n"
-	config += `	enable_radio_type_b = false` + "\n"
+	config += `	enable_radio_type_b = true` + "\n"
 	config += `	enable_radio_type6_g_hz = false` + "\n"
+	config += `	radio_type_a_parent_profile = "CUSTOM"` + "\n"
+	config += `	radio_type_a_radio_channels = "36,40,44,48,124,128,157,161"` + "\n"
+	config += `	radio_type_a_data_rates = "6,9,12,18,24,36,48,54"` + "\n"
+	config += `	radio_type_a_mandatory_data_rates = "6"` + "\n"
+	config += `	radio_type_a_power_threshold_v1 = -60` + "\n"
+	config += `	radio_type_a_rx_sop_threshold = "CUSTOM"` + "\n"
+	config += `	radio_type_a_min_power_level = 8` + "\n"
+	config += `	radio_type_a_max_power_level = 20` + "\n"
+	config += `	radio_type_a_channel_width = "20"` + "\n"
+	config += `	radio_type_a_custom_rx_sop_threshold = -70` + "\n"
+	config += `	radio_type_b_parent_profile = "HIGH"` + "\n"
+	config += `	radio_type_b_radio_channels = "1,6,11"` + "\n"
+	config += `	radio_type_b_data_rates = "1,2,5.5,6,9,11,12,18,24,36,48,54"` + "\n"
+	config += `	radio_type_b_mandatory_data_rates = "1,2"` + "\n"
+	config += `	radio_type_b_power_threshold_v1 = -60` + "\n"
+	config += `	radio_type_b_rx_sop_threshold = "CUSTOM"` + "\n"
+	config += `	radio_type_b_min_power_level = 8` + "\n"
+	config += `	radio_type_b_max_power_level = 20` + "\n"
+	config += `	radio_type_b_custom_rx_sop_threshold = -70` + "\n"
 	config += `}` + "\n"
 	return config
 }
